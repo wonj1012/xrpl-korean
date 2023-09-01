5863d4f5d2a26c57504cc76751c45c67fcdf6db4
wonj1012 <jjaa1012@gmail.com>
2023-09-01 13:56:30 +0900

    docs: Update .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/wonj/Projects/xrpl-korean/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/wonj/Projects/xrpl-korean/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/wonj/Projects/xrpl-korean/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/wonj/Projects/xrpl-korean/src/css/custom.css'),
-];
